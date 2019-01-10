221ad83c3141334119724f733ff22e84dd5adef0
Masbubul Karim <karimmdm1@gmail.com>
2019-01-10 05:45:15 -0500

    1/9/18

---
+++ APCSQuiz/APCSQuiz/app/src/main/java/com/example/imtia/apcsquiz/Main2Activity.kt
@@ -0,0 +1,13 @@
+package com.example.imtia.apcsquiz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import java.util.ArrayList
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
