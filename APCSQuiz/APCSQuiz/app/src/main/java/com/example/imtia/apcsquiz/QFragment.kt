5d821b9785218eb32c53ac096c0f494f850a9c54
imtiaznyc1 <imtiaznyc1@gmail.com>
2019-01-10 11:34:07 -0500

    updated qfragment file

--- APCSQuiz/APCSQuiz/app/src/main/java/com/example/imtia/apcsquiz/QFragment.kt
+++ APCSQuiz/APCSQuiz/app/src/main/java/com/example/imtia/apcsquiz/QFragment.kt
@@ -1,12 +1,17 @@
 package com.example.imtia.apcsquiz
 
 import android.content.Context
+import android.media.Image
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -27,6 +32,7 @@ class QFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var i:ImageView? = null
     private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,12 +40,16 @@ class QFragment : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        i = view?.findViewById<ImageView>(R.id.imageView)
+        Glide.with(this).load(R.drawable.betternextbtn).into(i)
         return inflater.inflate(R.layout.fragment_q, container, false)
     }
 
