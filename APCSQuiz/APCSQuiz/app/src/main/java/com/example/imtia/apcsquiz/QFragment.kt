309b7942fa03c7bc7ef34ab975d38d07538c7622
imtiaznyc1 <32941495+imtiaznyc1@users.noreply.github.com>
2019-01-12 22:47:39 -0500

    Updated QFile
    I need you to see why this fragment isn't loading up like when running on my phone it just closes the app

--- APCSQuiz/APCSQuiz/app/src/main/java/com/example/imtia/apcsquiz/QFragment.kt
+++ APCSQuiz/APCSQuiz/app/src/main/java/com/example/imtia/apcsquiz/QFragment.kt
@@ -1,17 +1,21 @@
 package com.example.imtia.apcsquiz
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.media.Image
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
+//import com.bumptech.glide.annotation.GlideModule
+//import com.bumptech.glide.module.AppGlideModule
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -29,11 +33,15 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class QFragment : Fragment() {
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     private var i:ImageView? = null
     private var listener: OnFragmentInteractionListener? = null
+    private var m: FragmentManager?=null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,13 +51,15 @@ class QFragment : Fragment() {
 
         }
 
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        i = view?.findViewById<ImageView>(R.id.imageView)
-        Glide.with(this).load(R.drawable.betternextbtn).into(i)
+        //i = view?.findViewById<ImageView>(R.id.imageView)
+        //Glide.with(this).load(R.drawable.betternextbtn).asGif().crossFade().into(i)
+        //Glide.with(this).load(R.drawable.betternextbtn).asGif().crossFade().into(i)
         return inflater.inflate(R.layout.fragment_q, container, false)
     }
 
@@ -58,6 +68,14 @@ class QFragment : Fragment() {
         listener?.onFragmentInteraction(uri)
     }
 
+    fun clickNextButton(v:View){
+        m = getFragmentManager()
+        var t:FragmentTransaction? = m?.beginTransaction()
+        var fq:QFragment = QFragment()
+        var fr:r = (r())
+        t?.detach(fq)
+        t?.attach(fr)
+    }
     override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is OnFragmentInteractionListener) {
@@ -89,6 +107,7 @@ class QFragment : Fragment() {
     }
 
     companion object {
+        fun newInstance() = QFragment()
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
@@ -98,13 +117,13 @@ class QFragment : Fragment() {
          * @return A new instance of fragment QFragment.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic
+        /*@JvmStatic
         fun newInstance(param1: String, param2: String) =
                 QFragment().apply {
                     arguments = Bundle().apply {
                         putString(ARG_PARAM1, param1)
                         putString(ARG_PARAM2, param2)
                     }
-                }
+                }*/
     }
 }
