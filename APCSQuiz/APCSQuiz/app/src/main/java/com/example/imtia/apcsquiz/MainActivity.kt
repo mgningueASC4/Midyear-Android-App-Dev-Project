b3c761cce4ca21fc1f86036c73163be20c08ddc6
Masbubul Karim <karimmdm1@gmail.com>
2019-01-17 09:52:19 -0500

    changed button methods

--- APCSQuiz/APCSQuiz/app/src/main/java/com/example/imtia/apcsquiz/MainActivity.kt
+++ APCSQuiz/APCSQuiz/app/src/main/java/com/example/imtia/apcsquiz/MainActivity.kt
@@ -9,8 +9,20 @@ import com.example.imtia.apcsquiz.R.layout.activity_main2
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    //widgets
     lateinit var testButton: Button
-    lateinit var
+    lateinit var algorithmBtn: Button
+    lateinit var loopBtn: Button
+    lateinit var arrayBtn: Button
+    lateinit var arrayListBtn: Button
+    lateinit var stringBtn: Button
+    lateinit var classesBtn: Button
+    lateinit var fieldsBtn: Button
+    lateinit var recursionBtn: Button
+    lateinit var bigOBtn: Button
+    lateinit var searchSortBtn: Button
+
+    var btnList : ArrayList<Button>  = ArrayList<Button>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +33,43 @@ class MainActivity : AppCompatActivity() {
 
     fun initWidgets(){
         testButton = TestButton
-        //stringButton = button1
+        algorithmBtn = ab_expressions
+        loopBtn = loops
+        arrayBtn = arrays
+        arrayListBtn = arraylists
+        stringBtn = strings
+        classesBtn = class_interface_inheritance_polymorphism
+        fieldsBtn = fields_methods_prameter
+        recursionBtn = recursion
+        bigOBtn = big_o
+        searchSortBtn = searchSort
+
+        btnList.add(algorithmBtn)
+        btnList.add(algorithmBtn)
+        btnList.add(loopBtn)
+        btnList.add(arrayBtn)
+        btnList.add(arrayListBtn)
+        btnList.add(stringBtn)
+        btnList.add(classesBtn)
+        btnList.add(fieldsBtn)
+        btnList.add(fieldsBtn)
+        btnList.add(recursionBtn)
+        btnList.add(bigOBtn)
+        btnList.add(searchSortBtn)
+
+        for(x in btnList){
+            var v = x.rootView
+            var t = x.text.toString()
+            x.setOnClickListener {
+                menuBtnAction(v, t)
+            }
+        }
+
     }
 
-    fun menuBtnAction(v: View){
+    fun menuBtnAction(v: View , topic:String){
         val i:Intent = Intent(this,Main2Activity::class.java)
+        i.putExtra("TOPIC", topic)
         startActivity(i)
     }
 }
